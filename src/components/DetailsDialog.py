849d36063ec59ba8eee20427bfcc3d2d5c0c01cc
marcodiri <marcodiri96@gmail.com>
2023-06-19 16:05:52 +0200

    Changed DetailsDialog widget

--- src/components/DetailsDialog.py
+++ src/components/DetailsDialog.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QTableWidgetItem
+from PyQt5.QtCore import Qt
 
 from .designer.Ui_DetailsDialog import Ui_DetailsDialog
 
@@ -9,3 +10,15 @@ class DetailsDialog(QDialog):
 
         self.ui = Ui_DetailsDialog()
         self.ui.setupUi(self)
+
+    def setDetails(self, details: dict):
+        self.ui.tableWidget.clearContents()
+        self.ui.tableWidget.setRowCount(len(details))
+
+        for i, (k, v) in enumerate(details.items()):
+            tag = QTableWidgetItem(str(k))
+            tag.setFlags(tag.flags() & ~Qt.ItemIsEditable)
+            value = QTableWidgetItem(str(v))
+            value.setFlags(value.flags() & ~Qt.ItemIsEditable)
+            self.ui.tableWidget.setItem(i, 0, tag)
+            self.ui.tableWidget.setItem(i, 1, value)
